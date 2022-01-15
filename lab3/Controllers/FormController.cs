640a75428a5c897414b54c684700fd46828ce6b2
utto2000 <bartek200039@gmail.com>
2022-01-15 15:18:01 +0100

    Add files via upload

---
+++ lab3/Controllers/FormController.cs
@@ -0,0 +1,28 @@
+﻿
+using lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+
+    public class FormController : Controller
+    {
+        public IActionResult Forms()
+        {
+            return View();
+        }
+
+        public IActionResult Display_Values(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Display_Values", model);
+            }
+
+            return View(model);
+        }
+
+    }
+
+
+}
